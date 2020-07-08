8415dfb3300406ac4e18da99c20dc3e0703d259f
peachmakkoli <lee.higgins.dev@gmail.com>
2020-07-07 22:16:57 -0700

    writes user data to database upon first logging in

--- lib/screens/loginpage/login.dart
+++ lib/screens/loginpage/login.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
-Future<String> signInWithGoogle() async {
+Future<FirebaseUser> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
       await googleSignInAccount.authentication;
@@ -19,9 +20,24 @@ Future<String> signInWithGoogle() async {
 
   assert(!user.isAnonymous);
   assert(await user.getIdToken() != null);
+  assert(await user.displayName != null);
+  assert(await user.email != null);
 
   final FirebaseUser currentUser = await _auth.currentUser();
   assert(user.uid == currentUser.uid);
 
-  return 'signInWithGoogle succeeded: $user';
+  final CollectionReference usersRef = Firestore.instance.collection('users');
+  final snapShot = await usersRef.document(user.uid).get();
+
+  if (snapShot == null || !snapShot.exists) {
+    var userData = {
+      'uid': user.uid,
+      'name': user.displayName,
+      'email': user.email,
+    };
+
+    await usersRef.document(user.uid).setData(userData);
+  }
+
+  return user;
 }
