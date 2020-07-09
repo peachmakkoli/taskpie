76cfadb05c715ef1595cfac388b984de7f14d298
peachmakkoli <lee.higgins.dev@gmail.com>
2020-07-08 19:08:14 -0700

    integrates default pie chart from syncfusion flutter charts package

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:suncircle/screens/landingpage/landingpage.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:suncircle/screens/landingpage/landingpage.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title, this.user}) : super(key: key);
@@ -16,8 +16,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   
+  Future<void> signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (error) {
+      print(error); // TODO: show dialog with error
+    }
+  }
   // TODO: Check if a new event has been added so the app can re-render 
   // the calendar circle
+
+  // function with setState that is called anytime the user refreshes the screen
+  // map tasks to chartData list
+  // query database for all tasks with reference to current user
 
   // int _counter = 0;
 
@@ -31,14 +42,6 @@ class _HomePageState extends State<HomePage> {
   //     _counter++;
   //   });
   // }
-
-  Future<void> signOut() async {
-    try {
-      await FirebaseAuth.instance.signOut();
-    } catch (error) {
-      print(error); // TODO: show dialog with error
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,6 +72,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      backgroundColor: Colors.white,
       body: Center(
         child: Column (
           mainAxisAlignment: MainAxisAlignment.center,
@@ -85,17 +89,46 @@ class _HomePageState extends State<HomePage> {
                     Text(snapshot.data['uid']),
                     Text(snapshot.data['name']),
                     Text(snapshot.data['email']),
+                    Container(
+                      height: 500,
+                      child: SfCircularChart(series: <CircularSeries>[
+                        PieSeries<ChartData, String>(
+                          enableSmartLabels: true,
+                          dataSource: [
+                            ChartData('', 25, '', Colors.white),
+                            ChartData('Task One', 38, 'Task One (38 mins)', Colors.yellow),
+                            ChartData('', 4, '', Colors.white),
+                            ChartData('Task Two', 2, 'Task Two (2 mins)', Colors.orange)
+                          ],
+                          pointColorMapper:(ChartData data,  _) => data.color,
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          radius: '90%',
+                          // explode: true,
+                          // explodeIndex: 0,
+                          dataLabelMapper: (ChartData data, _) => data.text,
+                          dataLabelSettings: DataLabelSettings(
+                            isVisible: true,
+                            useSeriesColor: true,
+                          ),
+                        )
+                      ]),
+                    ), // Container
                   ], // <Widget>
                 ); // Column
               },
             ), // Streambuilder
-            Container(
-              height: 500,
-              child: SfCartesianChart(),
-            ) // Container
-          ],
+          ], // <Widget>
         ),
       ), // Center
     ); // Scaffold
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y, this.text, [this.color]);
+  final String x;
+  final double y;
+  final String text;
+  final Color color;
 }
